873025c7a7bd7b5908ebb3cf71b9f81ff3b08a0f
billerickson <billerickson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-27 22:19:34 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/gallery-metabox/trunk@456507 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gallery-metabox.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: Gallery Metabox
+Plugin URI: http://www.billerickson.net
+Description: Displays all the post's attached images on the Edit screen
+Author: Bill Erickson
+Version: 1.0
+Author URI: http://www.billerickson.net
+*/
+
+/**
+ * Translations
+ *
+ */
+function be_gallery_metabox_translations() {
+	load_plugin_textdomain( 'gallery-metabox', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+add_action( 'init', 'be_gallery_metabox_translations' );
+
+
+/**
+ * Add the Metabox
+ * @since 1.0
+ */
+function be_gallery_metabox_add() {
+	// Filterable metabox settings. 
+	$post_types = apply_filters( 'be_gallery_metabox_post_types', array( 'post', 'page') );
+	$context = apply_filters( 'be_gallery_metabox_context', 'normal' );
+	$priority = apply_filters( 'be_gallery_metabox_priority', 'high' );
+	
+	// Loop through all post types
+	foreach( $post_types as $post_type ) {
+		
+		// Get post ID
+		if( isset( $_GET['post'] ) ) $post_id = $_GET['post'];
+		elseif( isset( $_POST['post_ID'] ) ) $post_id = $_POST['post_ID'];
+		if( !isset( $post_id ) ) return;
+		
+		// Granular filter so you can limit it to single page or page template
+		if( apply_filters( 'be_gallery_metabox_limit', true, $post_id ) )
+			add_meta_box( 'be_gallery_metabox', __( 'Gallery Images', 'gallery-metabox' ), 'be_gallery_metabox', $post_type, $context, $priority );
+
+	}
+}
+add_action( 'add_meta_boxes', 'be_gallery_metabox_add' );
+
+/**
+ * Build the Metabox
+ * @since 1.0
+ * @param object $post
+ *
+ */
+function be_gallery_metabox( $post ) {
+	$args = array(
+		'post_type' => 'attachment',
+		'post_status' => 'inherit',
+		'post_parent' => $post->ID,
+		'post_mime_type' => 'image',
+		'posts_per_page' => '-1',
+		'order' => 'ASC',
+		'orderby' => 'menu_order',
+	);
+	$args = apply_filters( 'be_gallery_metabox_args', $args );
+	
+	$intro = '<p><a href="media-upload.php?post_id=' . $post->ID .'&amp;type=image&amp;TB_iframe=1&amp;width=640&amp;height=715" id="add_image" class="thickbox" title="' . __( 'Add Image', 'galllery-metabox' ) . '">' . __( 'Upload Images', 'gallery-metabox' ) . '</a> | <a href="media-upload.php?post_id=' . $post->ID .'&amp;type=image&amp;tab=gallery&amp;TB_iframe=1&amp;width=640&amp;height=715" id="manage_gallery" class="thickbox" title="' . __( 'Manage Gallery', 'gallery-metabox' ) . '">' . __( 'Manage Gallery', 'gallery-metabox' ) . '</a></p>';
+	echo apply_filters( 'be_gallery_metabox_intro', $intro );
+
+	
+	$loop = new WP_Query( $args );
+	if( !$loop->have_posts() )
+		echo '<p>No images.</p>';
+			
+	while( $loop->have_posts() ): $loop->the_post(); global $post;
+		$thumbnail = wp_get_attachment_image_src( $post->ID, apply_filters( 'be_gallery_metabox_image_size', 'thumbnail' ) );
+		echo '<img src="' . $thumbnail[0] . '" alt="' . get_the_title() . '" /> ';
+	endwhile; wp_reset_query();
+
+}
+
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Gallery Metabox ===
+Contributors: billerickson
+Tags: gallery, image, images, metabox
+Requires at least: 3.0
+Tested up to: 3.1.3
+Stable tag: trunk
+
+Displays all the post's attached images on the Edit screen
+
+== Description ==
+
+I use the WordPress Gallery a lot on websites I build. It's a wonderful tool, but it's hard to find. Instead of telling clients "Click the first icon next to Upload/Insert, then click the gallery tab", I created this simple plugin to display all the attached images in a metabox.
+
+If you're a developer, there's a ton of filters in here to customize it specifically to your needs:
+*	`be_gallery_metabox_post_types` is an array of post types the metabox should be visible on. Default: array( 'post', 'page' )
+*	`be_gallery_metabox_limit` allows you to further refine your metabox by limiting it to specific pages or page templates ( [example](https://gist.github.com/1320990) ). Default: true
+*	`be_gallery_metabox_context` whether to display it in the main area or sidebar. Default: normal
+*	`be_gallery_metabox_priority` priority of metabox. Default: high
+*	`be_gallery_metabox_args` query args for image listing. Useful if you're [adding custom fields to media library](http://www.billerickson.net/wordpress-add-custom-fields-media-gallery/) ( [example](https://gist.github.com/1321001) )
+*	`be_gallery_metabox_intro` Text displayed above image listing. Default: Upload Image | Manage Gallery
+*	`be_gallery_metabox_image_size` The image size displayed in the metabox. Default: thumbnail
+
+== Changelog ==
+
+= 1.0 =
+* Release of plugin
+
